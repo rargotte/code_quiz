e7611ee6b962cf9c04d77aed8ede18e73845d171
Condor Ito <rargotte@hotmail.com>
2021-11-29 20:20:34 -0600

    Initial Version code_quiz

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # code_quiz
+
+Quiz based on javascript questions to evaluate your knowledge
+
+Requirements
+------------
+
+No specific requirements.
+
+This application was developed using HTML, CSS & Javascript.
+
+Installation
+------------
+
+No installation needed.
+This is a online quiz to have fun with.
+
+
+Usage
+------------
+The introduction screen will provide some details to start the quiz.
+
+Once you press the start button all you need to do is to select the right answer by clicking on it.
+
+At the end the appliation will provide your score and will ask for your initials. If you managed to beat the previous score you will be on the score board.
+
+You can delete the score board or go back to start the same quiz again.
+
+Here is the starting screen:
+
+<img src= "img/start.jpg" alt= "Starting screen">
+
+This is one of the questions of the quiz. Once you provide your answer the next screen will show the next question and the result of your answer.
+
+
+<img src= "img/1st Question.png" alt= "Screen with 1st Question">
+
+Here the application shows your score and asks for your inititals.
+Please provide and then select the go back button to re-start the quiz or "Clear Scores" to initialize the score board.
+
+<img src= "img/scorescreen.jpg" alt= "Screen with the result of your score">
+
+If you want to add more questions and answer is very easy:
+
+1. In the script.js file add a new "quizx" object. Just copy the first one and change the number of the question. In this case we are adding question number 5:
+
+var quiz5 = {
+    question: "What is the name of this application?",
+    answers: {
+        answer1: "1. code_quiz",
+        answer2: "2. quiz_code",
+        answer3: "3. code_code"
+    },
+
+        correctAnswer: "b_1",
+
+};
+
+2. And then add the new object in the array of questions:
+
+var quiz = [quiz1, quiz2, quiz3, quiz4, quiz5];
+
+3. The new question should be displayed next time you run the application.
+
+
+## License
+Copyright (c) 11/06/2021 _Regulo Argotte_
---
+++ script.js
@@ -0,0 +1,374 @@
+const penalization = 5;
+
+var startButton = document.querySelector(".startButton");
+var container = document.querySelector(".container");
+var score = 0;
+var secondsLeft = 575;
+var markup = container.outerHTML;
+
+
+
+var quiz1 = {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: {
+        answer1: "1. <javascript>",
+        answer2: "2. <scripting>",
+        answer3: "3. <script>"
+    },
+    correctAnswer: "b_3",
+};
+var quiz2 = {
+    question: "How do you call a function named 'myFunction'?",
+    answers: {
+        answer1: "1. myFunction()",
+        answer2: "2. call function myFunction()",
+        answer3: "3. call myFunction()"
+    },
+    correctAnswer: "b_1",
+};
+
+var quiz3 = {
+    question: "How do you create a function in JavaScript?",
+    answers: {
+        answer1: "1. function: myfunction()",
+        answer2: "2. function = myfunction()",
+        answer3: "3. function myfunction()"
+    },
+    correctAnswer: "b_3",
+};
+
+var quiz4 = {
+    question: "How to write an IF statement in JavaScript?",
+    answers: {
+        answer1: "1.  if i = 5",
+        answer2: "2.  if (i == 5)",
+        answer3: "3.  if i == 5 then"
+    },
+    correctAnswer: "b_2",
+};
+
+var quiz = [quiz1, quiz2, quiz3, quiz4];
+
+function initializeVar() {
+    secondsLeft = 1175;
+    score = 0;
+    container = document.querySelector(".container");
+};
+
+
+//Function to clean up the screen.
+function cleanupScreen() {
+    container.innerHTML = '';
+};
+
+//Function to start the timer
+function startTimer() {
+
+    //Create the element for the timer
+    var timerElement = document.createElement("div");
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        console.log(secondsLeft);
+        if (secondsLeft < 0) { secondsLeft = 0 }
+        timerElement.textContent = "TIME: " + secondsLeft;
+        timerElement.setAttribute("class", "timer");
+        container.appendChild(timerElement);
+
+        if (secondsLeft === 0) {
+            //Stops execution of action 
+            clearInterval(timerInterval);
+            //Calls function to end quiz
+            endQuiz();
+        }
+    }, 1000)
+}
+function showMessage(string) {
+    var inactiveMessage = document.getElementsByName("resultMessage");
+    for (var i = 0; i < inactiveMessage.length; i++) {
+        inactiveMessage[i].innerHTML = '';
+        inactiveMessage[i].outerHTML = '';
+    }
+    var lineElement = document.createElement("hr");
+    var messageResult = document.createElement("h2");
+    var messageText = document.createTextNode(string);
+    lineElement.setAttribute("style", "margin-left: 16%");
+    lineElement.setAttribute("name", "resultMessage");
+    messageResult.setAttribute("id", "resultMessage");
+    messageResult.appendChild(messageText);
+    lineElement.append(messageResult);
+    container.appendChild(lineElement);
+}
+
+function showNextQuestion(active) {
+    var allQuestionsAndAnswers = document.querySelectorAll('[data-id]');
+    nextQuestion = 1 + parseInt(active, 10);
+    if (nextQuestion < quiz.length) {
+        //Change status hide / show
+        for (var i = 0; i < allQuestionsAndAnswers.length; i++) {
+            if (allQuestionsAndAnswers[i].getAttribute("data-id") == active) {
+                //Hide active active question
+                if (allQuestionsAndAnswers[i].getAttribute("class") == "showQuestion") {
+                    allQuestionsAndAnswers[i].setAttribute("class", "hideQuestion");
+                    //Hide active buttons (answers)
+                } else if (allQuestionsAndAnswers[i].getAttribute("class") == "showButton") {
+                    allQuestionsAndAnswers[i].setAttribute("class", "hideButton");
+                }
+            } else { //Show next Q&A..
+
+                if (allQuestionsAndAnswers[i].getAttribute("data-id") == nextQuestion) {
+                    if (allQuestionsAndAnswers[i].getAttribute("class") == "hideQuestion") {
+                        allQuestionsAndAnswers[i].setAttribute("class", "showQuestion");
+                    } else if (allQuestionsAndAnswers[i].getAttribute("class") == "hideButton") {
+                        allQuestionsAndAnswers[i].setAttribute("class", "showButton");
+                    }
+                }
+            }
+        }
+    } else {
+        secondsLeft = 0;
+    }
+}
+
+function handleUserAnswer(userEvent) {
+    var userInput = userEvent.target;
+
+    if (userInput.id === "Back") {
+
+        resetQuiz();
+
+    } else if (userInput.id === "Clear") {
+
+        clearScores();
+
+    } else {
+
+        var rightAnswer = quiz[userInput.getAttribute("data-id")].correctAnswer;
+
+        if (userInput.id === rightAnswer) {
+            score = score + 5;
+            showMessage("Correct!");
+        } else {
+            secondsLeft = secondsLeft - penalization;
+            showMessage("Wrong!");
+        }
+        showNextQuestion(userInput.dataset.id);
+    }
+}
+
+
+
+//Function to get user input
+function getUserAnswers(listClass) {
+    var buttonLists = document.getElementsByClassName(listClass);
+    //Create the event listener to all buttonList groups
+    for (var i = 0; i < buttonLists.length; i++) {
+
+        buttonLists[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            var isButton = event.target.nodeName === 'BUTTON';
+            if (!isButton) {
+                return
+            } else {
+                handleUserAnswer(event);
+            }
+        })
+    }
+}
+
+
+
+
+
+function renderQuestion() {
+    //Create the element for the question
+
+    var questionText = "";
+
+    for (var i = 0; i < quiz.length; i++) {
+        var questionElement = document.createElement("h2");
+        questionText = document.createTextNode(quiz[i].question);
+        questionElement.appendChild(questionText);
+        //Showing only the first question. Others are hidden
+        if (i == 0) {
+            questionElement.setAttribute("class", "showQuestion");
+        } else {
+            questionElement.setAttribute("class", "hideQuestion");
+        }
+        questionElement.setAttribute("data-id", i);
+        container.appendChild(questionElement);
+
+        //Create the list of buttons for possible answers
+        answersObj = quiz[i].answers;
+        j = 0. // counter of possible answers
+        groupElement = document.createElement("group");
+        groupElement.setAttribute("class", "buttonList");
+        groupElement.setAttribute("data-id", i);
+        for (var property in answersObj) {
+            buttonElement = document.createElement("button");
+            buttonText = document.createTextNode(`${answersObj[property]}`)
+            j = j + 1;
+            buttonElement.setAttribute("ID", "b_" + j);
+            //Showing only answers of first question. Others are hidden.
+            if (i == 0) {
+                buttonElement.setAttribute("class", "showButton");
+            } else {
+                buttonElement.setAttribute("class", "hideButton");
+            }
+            buttonElement.setAttribute("data-id", i);
+            buttonText = document.createTextNode(`${answersObj[property]}`)
+            buttonElement.appendChild(buttonText);
+            groupElement.appendChild(buttonElement);
+
+        }
+
+        container.appendChild(groupElement);
+    }
+
+}
+
+function deleteQuestionsandAnswers() {
+    var eraseQuestions = document.querySelectorAll('[data-id]');
+    for (var i = 0; i < eraseQuestions.length; i++) {
+        eraseQuestions[i].remove();
+    }
+}
+//Displaying last result answer, providing score and asking for Initials.
+function getInitials() {
+    var allDoneElement = document.createElement("h2");
+    allDoneText = document.createTextNode("All done!");
+    allDoneElement.appendChild(allDoneText);
+
+    var yourFinalScore = document.createElement("p");
+    yourFinalScoreText = document.createTextNode("Your final Score is: " + score);
+    yourFinalScore.setAttribute("class", "paragraph")
+    yourFinalScore.appendChild(yourFinalScoreText);
+
+    var enterInitials = document.createElement("label")
+    enterInitialsText = document.createTextNode("Enter Inititals: ");
+    enterInitials.setAttribute("for", "Enter Initials");
+    enterInitials.appendChild(enterInitialsText);
+
+    var textArea = document.createElement("textarea");
+    textArea.setAttribute("id", "initials");
+
+    var submitInitials = document.createElement("button");
+    submitInitials.setAttribute("id", "save");
+    submitInitials.setAttribute("class", "singleButton");
+    submitInitialsText = document.createTextNode("Submit");
+    submitInitials.appendChild(submitInitialsText);
+
+    container.prepend(submitInitials);
+    container.prepend(textArea);
+    container.prepend(enterInitials);
+    container.prepend(yourFinalScore);
+    container.prepend(allDoneElement);
+
+    var saveButton = document.getElementById("save");
+    saveButton.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        var currentUser = {
+            grade: score,
+            initials: textArea.value
+        }
+        var userGrade = JSON.parse(localStorage.getItem("userGrade"));
+        if ((userGrade == null) || (userGrade !== null && userGrade.grade < score)) {
+
+            localStorage.setItem("userGrade", JSON.stringify(currentUser));
+        }
+        showScore();
+    })
+
+}
+
+
+
+
+
+function showScore() {
+    cleanupScreen();
+    var titleScoresElement = document.createElement("h2");
+    titleScoresText = document.createTextNode("Highscores");
+    titleScoresElement.appendChild(titleScoresText);
+    container.appendChild(titleScoresElement);
+
+    var highGrade = JSON.parse(localStorage.getItem("userGrade"));
+    if (highGrade !== null) {
+        var highScoreElement = document.createElement("h3");
+        highScoreText = document.createTextNode(highGrade.initials + "   " + highGrade.grade);
+        highScoreElement.appendChild(highScoreText);
+        container.appendChild(highScoreElement);
+    }
+
+    var buttonBack = document.createElement("button");
+    buttonBack.setAttribute("id", "Back");
+    buttonBack.setAttribute("class", "singleButton");
+    buttonBackText = document.createTextNode("Go Back");
+    buttonBack.appendChild(buttonBackText);
+
+    var buttonClearScore = document.createElement("button");
+    buttonClearScore.setAttribute("id", "Clear");
+    buttonClearScore.setAttribute("class", "singleButton");
+    buttonClearScoreText = document.createTextNode("Clear HighScores");
+    buttonClearScore.appendChild(buttonClearScoreText);
+
+    var listOfButtons = document.createElement("div");
+    listOfButtons.setAttribute("class", "scoreButtons");
+    listOfButtons.appendChild(buttonBack);
+    listOfButtons.appendChild(buttonClearScore);
+
+
+    container.appendChild(listOfButtons);
+    getUserAnswers("scoreButtons");
+
+}
+
+function clearScores() {
+
+    localStorage.clear("userGrade");
+    showScore();
+
+
+}
+
+
+
+
+
+
+//Function to end Quiz
+function endQuiz() {
+    deleteQuestionsandAnswers();
+    getInitials();
+}
+
+function startQuiz() {
+
+    initializeVar();
+    cleanupScreen();
+    startTimer();
+    renderQuestion();
+    getUserAnswers("buttonList");
+
+}
+
+function resetQuiz() {
+    var bodyElement = document.querySelector('body');
+    container.remove();
+    originalBody = markup + bodyElement.outerHTML;
+    bodyElement.outerHTML = originalBody;
+    startButton = document.querySelector(".startButton");
+    startButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        startQuiz();
+
+    })
+}
+
+startButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    startQuiz();
+})
+
+
+
